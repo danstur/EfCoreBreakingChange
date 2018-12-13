191685cb03b343fa657ea9ca3a4177ab862a1c51
DaS <daniel.sturm@softwaretechnik.at>
2018-12-13 11:57:56 +0100

    Demonstrate issue.

---
+++ EfCoreBreakingChange/CreateDb.sql
@@ -0,0 +1,20 @@
+﻿use master;
+
+CREATE DATABASE TestDb
+GO
+
+CREATE TABLE OutboundDelivery(
+	[Id] int IDENTITY(1,1) NOT NULL,
+	[SenderId] int not null,
+	CONSTRAINT [PK_FOO] PRIMARY KEY ([Id]),
+	CONSTRAINT [FK_BAR_FOO] 
+		FOREIGN KEY ([SenderId])     
+		REFERENCES [Site](Id)  
+);
+
+CREATE TABLE [Site] (
+	[Id] int IDENTITY(1,1) NOT NULL,
+	[SapSiteId] nvarchar(4) NOT NULL,
+	CONSTRAINT [PK_BAR] PRIMARY KEY ([Id])
+)
+GO
---
+++ EfCoreBreakingChange/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EfCoreBreakingChange
+{
+    public sealed class MyContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=(local);Database=TestDb;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration(new OutboundDeliveryTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new SiteTypeConfiguration());
+        }
+    }
+
+    public sealed class OutboundDelivery
+    {
+        public int Id { get; private set; }
+
+        public Site Sender { get; private set; }
+    }
+
+    public sealed class OutboundDeliveryTypeConfiguration : IEntityTypeConfiguration<OutboundDelivery>
+    {
+        public void Configure(EntityTypeBuilder<OutboundDelivery> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.HasOne(o => o.Sender)
+                .WithMany()
+                .HasForeignKey("SenderId")
+                .IsRequired();
+        }
+    }
+
+    public class Site
+    {
+        public string SiteId { get; private set; }
+    }
+
+    public sealed class SiteTypeConfiguration : IEntityTypeConfiguration<Site>
+    {
+        public void Configure(EntityTypeBuilder<Site> builder)
+        {
+            builder.Property<int>("Id");
+            builder.HasKey("Id");
+            builder.Property(p => p.SiteId)
+                .HasColumnName("SapSiteId");
+        }
+    }
+
+    public static class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            using (var ctx = new MyContext())
+            {
+                var deliveries = await ctx.Set<OutboundDelivery>().ToListAsync();
+            }
+        }
+    }
+}
